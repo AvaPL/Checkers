85530512a1034b5a77e027f5a6b1f2db701dcbf0
Paweł Cembaluk <241574@student.pwr.edu.pl>
2019-02-24 13:50:42 +0100

    Added better crown animation with particles.

--- Checkers/Assets/Scripts/Pawns/PawnProperties.cs
+++ Checkers/Assets/Scripts/Pawns/PawnProperties.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PawnProperties : MonoBehaviour
 {
     public PawnColor PawnColor;
     public bool IsKing;
+    public float CrownHeight;
+    public float CrownAppearanceSmoothing;
+    public float PositionDifferenceTolerance;
     public GameObject Crown;
+    public GameObject PromotionParticles;
 
     public TileIndex GetTileIndex()
     {
@@ -13,9 +18,28 @@ public class PawnProperties : MonoBehaviour
 
     public void PromoteToKing()
     {
-        //TODO: Add particle effect.
         IsKing = true;
-        Instantiate(Crown, transform);
+        CreatePromotionParticles();
+        StartCoroutine(AddCrown());
         Debug.Log("Pawn promoted to king.");
+    }
+
+    private void CreatePromotionParticles()
+    {
+        GameObject instantiatedParticles = Instantiate(PromotionParticles, transform);
+        var particlesDuration = PromotionParticles.GetComponent<ParticleSystem>().main.duration;
+        Destroy(instantiatedParticles, particlesDuration * 5);
+    }
+
+    IEnumerator AddCrown()
+    {
+        var crownTransform = Instantiate(Crown, transform).transform;
+        Vector3 targetPosition = crownTransform.position + Vector3.up * CrownHeight;
+        while (Vector3.Distance(crownTransform.position, targetPosition) > PositionDifferenceTolerance)
+        {
+            crownTransform.position = Vector3.Lerp(crownTransform.position, targetPosition,
+                CrownAppearanceSmoothing * Time.deltaTime);
+            yield return null;
+        }
     }
 }

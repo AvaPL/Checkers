e403603504202670fd709b4e41a5d9eb8694b9a4
Paweł Cembaluk <241574@student.pwr.edu.pl>
2019-04-20 15:40:30 +0200

    Added simple turn text animation.

--- Checkers/Assets/Scripts/UI/TurnTextChanger.cs
+++ Checkers/Assets/Scripts/UI/TurnTextChanger.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 public class TurnTextChanger : MonoBehaviour
 {
     private TextMeshProUGUI turnText;
+    private Animator textAnimator;
 
     private void Start()
     {
         turnText = GetComponent<TextMeshProUGUI>();
+        textAnimator = GetComponent<Animator>();
     }
 
     public void ChangeTurnText(PawnColor pawnColor)
     {
         turnText.text = pawnColor.ToString().ToUpper() + "'S TURN";
+        textAnimator.Play("TurnTextAnimation");
     }
 }
